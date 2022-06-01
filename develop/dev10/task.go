5e5ab179069cff2203e869046a3359c741f5d316
Kirya Voronkov <kirya.voronkov.73@mail.ru>
2022-06-01 15:06:32 +0300

    some fix

---
+++ develop/dev10/task.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Telnet struct { // структура с полученными данными
+	host    string
+	port    string
+	timeout time.Duration
+}
+
+func connect() *Telnet {
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "timeout")
+
+	flag.Parse() // парсим флаги
+	args := flag.Args()
+	if len(args) < 2 {
+		panic("invalid params")
+	}
+	a := &Telnet{
+		host:    args[0],
+		port:    args[1],
+		timeout: timeout,
+	}
+	return a
+
+}
+
+func send(conn net.Conn, sigChan chan os.Signal, errChan chan error) {
+	for {
+		reader := bufio.NewReader(os.Stdin)  // читаем из консоли
+		text, err := reader.ReadString('\n') // читаем до разделителя
+		if err != nil || err != io.EOF {     // если файл для чтения кончился
+			sigChan <- syscall.SIGQUIT
+			return
+		}
+		errChan <- err
+
+		fmt.Fprintf(conn, text+"\n") // отправляем в сокет
+	}
+}
+
+func read(conn net.Conn, sigChan chan os.Signal, errChan chan error) {
+
+	reader := bufio.NewReader(conn)      // читаем из канала
+	text, err := reader.ReadString('\n') // читаем до разделителя
+	if err != nil || err != io.EOF {     // если файл для чтения кончился
+		sigChan <- syscall.SIGQUIT
+		return
+	}
+	errChan <- err
+
+	fmt.Print("Message Received:", text) // выводим полученое сообщение в консоль
+}
+
+func main() {
+	a := connect()
+	sigChan := make(chan os.Signal, 1) // канал для отправки сигналов о завершении работы клиента
+	errChan := make(chan error, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM) // отлавливаем сигналы о завершении работы
+	address := net.JoinHostPort(a.host, a.port)             // объединяем хост и порт
+	conn, err := net.DialTimeout("tcp", address, a.timeout) // подключаемся к сети
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go send(conn, sigChan, errChan)
+	go read(conn, sigChan, errChan)
+
+	select {
+	case <-sigChan:
+		fmt.Println("Telnet client is close")
+		conn.Close()
+	case err := <-errChan:
+		log.Println(err)
+		return
+	}
+
+}
--- develop/dev11/cmd/main.go
+++ develop/dev11/cmd/main.go
@@ -2,32 +2,25 @@ package main
 
 import (
 	"L2/develop/dev11/server"
+	"flag"
 	"log"
 	"net/http"
-	"os"
 )
 
-func main(){
+func main() {
+	P := flag.String("p", "80", "port")
+	flag.Parse()
 
-	mux:=http.NewServeMux()
+	mux := http.NewServeMux()
 
-	handler:=server.NewHandler()
+	handler := server.NewHandler()
 
 	handler.Register(mux)
 
 	muxWithLogger := server.Logging(mux)
 
-	addr := ":8080"
-	func() {
-		temp := os.Getenv("PORT")
-		if temp != "" {
-			addr = temp
-		}
-	}()
-
-
-	log.Printf("Server is listening on: %s\n", addr)
-	err:= http.ListenAndServe(addr, muxWithLogger)
+	log.Printf("Server is listening on: %s\n", *P)
+	err := http.ListenAndServe("localhost:"+*P, muxWithLogger)
 	if err != nil {
 		log.Fatal(err)
 	}
